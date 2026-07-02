ab4961de3dfabb1f89d549967c7759f6a24d094c
Gabriel França <gabrielgfranca7@proton.me>
2026-07-02 19:06:32 -0300

    feat: insert function

--- src/main/java/br/com/gabriel/dao/BookDAO.java
+++ src/main/java/br/com/gabriel/dao/BookDAO.java
@@ -3,6 +3,8 @@ package br.com.gabriel.dao;
 import br.com.gabriel.model.Book;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,8 +15,20 @@ public class BookDAO {
         this.connection = connection;
     }
 
-    void insert(Book book) {
+    void insert(Book book) throws SQLException {
+        String sql = "INSERT INTO books (id, title, author, publication_year, genre, isbn, is_read, synopsis) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement ps = connection.prepareStatement(sql);
 
+        ps.setInt(1, book.getId());
+        ps.setString(2, book.getTitle());
+        ps.setString(3, book.getAuthor());
+        ps.setInt(4, book.getYear());
+        ps.setString(5, book.getGenre());
+        ps.setString(6, book.getIsbn());
+        ps.setBoolean(7, book.isRead());
+        ps.setString(8, book.getSynopsis());
+
+        ps.executeUpdate();
     }
 
     List<Book> listAll(int pages) {
